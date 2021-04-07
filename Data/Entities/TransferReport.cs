fde95dae0e5e4bee0123e8ed0fac4f8fc787de45
Julian Pratt <julian.pratt@me.com>
2021-04-07 15:28:04 +0100

    Still trying to knock out tabs

--- Data/Entities/TransferReport.cs
+++ Data/Entities/TransferReport.cs
@@ -124,7 +124,7 @@ namespace ReportDist.Data
                 {
                     string temp = "&#x" + String.Format("{0:X}", Convert.ToUInt32(c)) + ";";
                     if (temp == "&#x9;") sb.Append(" ");
-                    if (temp != "&#x1F;" && temp != "&#xB;" && temp != "&#x1;" && temp != "&#xDBC0;" && temp != "&#xDC79;" 
+                    else if (temp != "&#x1F;" && temp != "&#xB;" && temp != "&#x1;" && temp != "&#xDBC0;" && temp != "&#xDC79;" 
                      && temp != "&#xC;"  && temp != "&#x2;" && temp != "&#xA;" && temp != "&#xD;"   ) 
                         sb.Append(temp);
                 }                      
